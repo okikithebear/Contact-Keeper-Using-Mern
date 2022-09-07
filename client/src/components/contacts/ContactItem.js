3aef1f04bfbe5c0c8d726b4fe2361cc4adfd4237
Musa Liman <musaliman27@gmail.com>
2022-09-07 15:44:26 +0100

    third commit

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 export const ContactItem = ({contact}) => {
   const { id, name, email, phone, type } = contact;
@@ -6,10 +7,26 @@ export const ContactItem = ({contact}) => {
     return (
     <div className="card bg-light">
         <h3 className='text-primary text-left'>
-            {name}{' '}<span className={'badge ' + (type === 'prodfessional' ? 'badge-sucess' : 'badge-primary')}>{type.charAt(0).toUpperCase() + type.slice(1)}</span>
+            {name}{' '}<span style={{float: 'right'}} className={'badge ' + (type === 'professional' ? 'badge-success' : 'badge-primary')}>{type.charAt(0).toUpperCase() + type.slice(1)}</span>
          </h3>
+        <ul className='list'>
+          {email && (<li>
+           <i className='fas fa-envelope-open'></i> {email} 
+          </li>)}
+          {phone && (<li>
+           <i className='fas fa-phone'></i> {phone} 
+          </li>)}
+        </ul>
+        <p>
+          <button className='btn btn-dark btn-sm'>Edit</button>
+          <button className='btn btn-danger btn-sm'>Delete</button>
+        </p>
     </div>
   );
 };
+
+ContactItem.propTypes ={
+  contact: PropTypes.object.isRequired
+}
 
 export default ContactItem;
--- client/src/context/Contact/contactReducer.js
+++ client/src/context/Contact/contactReducer.js
@@ -11,71 +11,15 @@ import {
     CLEAR_CONTACTS
   } from '../Types';
   
-  const contactReducer = (state, action) => {
-    switch (action.type) {
-      case GET_CONTACTS:
-        return {
-          ...state,
-          contacts: action.payload
-        };
-      case ADD_CONTACT:
-        return {
-          ...state,
-          contacts: [action.payload, ...state.contacts]
-        };
-      case UPDATE_CONTACT:
-        return {
-          ...state,
-          contacts: state.contacts.map((contact) =>
-            contact._id === action.payload._id ? action.payload : contact
-          )
-        };
-      case DELETE_CONTACT:
-        return {
-          ...state,
-          contacts: state.contacts.filter(
-            (contact) => contact._id !== action.payload
-          )
-        };
-      case CLEAR_CONTACTS:
-        return {
-          ...state,
-          contacts: null,
-          filtered: null,
-          error: null,
-          current: null
-        };
-      case SET_CURRENT:
-        return {
-          ...state,
-          current: action.payload
-        };
-      case CLEAR_CURRENT:
-        return {
-          ...state,
-          current: null
-        };
-      case FILTER_CONTACTS:
-        return {
-          ...state,
-          filtered: state.contacts.filter(({ name, email }) => {
-            const testString = `${name}${email}`.toLowerCase();
-            return testString.includes(action.payload.toLowerCase());
-          })
-        };
-      case CLEAR_FILTER:
-        return {
-          ...state,
-          filtered: null
-        };
-      case CONTACT_ERROR:
-        return {
-          ...state,
-          error: action.payload
-        };
-      default:
-        throw new Error(`Unsupported type of: ${action.type}`);
+  export default(state, action) => {
+    switch(action.type){
+    case ADD_CONTACT:
+      return{
+        ...state,
+        contacts:[...state.contacts, action.payload]
+      };
+      default: 
+      return state;
     }
-  };
+  }
   
-  export default contactReducer;
