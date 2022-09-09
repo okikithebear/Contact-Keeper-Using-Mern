896bfb71cfe414c6e18345e6fddecef2e79b4512
Musa Liman <musaliman27@gmail.com>
2022-09-09 06:28:14 +0100

    fuction Alert

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,22 @@ import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Register from './components/Auth/Register';
 import Login from './components/Auth/Login';
+import Alerts from './components/layout/Alerts';
 import ContactState from './context/Contact/ContactState';
 import AuthState from './context/Auth/AuthState';
+import AlertState from './context/Alert/AlertState';
 import './App.css';
 
 const App = () => {
   return (
     <AuthState>
     <ContactState>
+    <AlertState>
     <Router>
     <Fragment>
       <Navbar/>
     <div className="container">
+      <Alerts/>
 <Routes>
 <Route exact path="/" element={<Home />} />
 <Route exact path="/about" element={<About />} />
@@ -26,6 +30,7 @@ const App = () => {
     </div>
     </Fragment>
     </Router>
+    </AlertState>
     </ContactState>
     </AuthState>
   );
